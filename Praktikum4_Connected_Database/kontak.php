8606615c636c36e67f682cc5e0d5b58de7179522
Alwan-1 <alwanmudhaffar392@gmail.com>
2025-12-10 14:47:12 +0700

    Create kontak.php

---
+++ Praktikum4_Connected_Database/kontak.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="UTF-8">
+<title>GymNess- Kontak</title>
+
+<style>
+body { font-family: Arial; background:#f5f5f5; margin:0; }
+.hero { background:#222; color:white; padding:35px; text-align:center; }
+.navbar { background:#333; text-align:center; }
+.navbar a { color:white; padding:15px 25px; display:inline-block; text-decoration:none; }
+.navbar a:hover { background:#555; }
+.container { width:80%; margin:30px auto; }
+.card { background:white; padding:25px; border-radius:10px; box-shadow:0 4px 12px rgba(0,0,0,0.1); }
+.contact li { margin-bottom:10px; }
+.footer { background:#222; color:white; text-align:center; padding:15px; margin-top:40px; }
+</style>
+
+</head>
+<body>
+
+<div class="hero">
+    <h1>GymFit</h1>
+    <p>Website Pemesanan Gym Online</p>
+</div>
+
+<div class="navbar">
+    <a href="indexs.php">Beranda</a>
+    <a href="pemesanan2.php">Pemesanan</a>
+    <a href="jadwal.php">Jadwal</a>
+    <a href="kontak.php">Kontak</a>
+    <a href="gambar2.php">Gambar</a>
+</div>
+
+<div class="container">
+    <div class="card">
+        <h2>Kontak Kami</h2>
+
+        <ul class="contact">
+            <li><strong>Email:</strong> admin@GymNess.com</li>
+            <li><strong>Telepon:</strong> 0858-5652-1232</li>
+            <li><strong>Alamat:</strong> Jl. Kenanga Kalijambe Tarub No. 10, jawa tengah</li>
+        </ul>
+    </div>
+</div>
+
+<div class="footer">©GymNess - Salam Sehat ><</div>
+
+</body>
+</html>
