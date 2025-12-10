3ea1b8a809dcda77c5ed3725a7fb0e1d636fa3e3
Alwan-1 <alwanmudhaffar392@gmail.com>
2025-12-10 14:54:13 +0700

    Create beranda.php

---
+++ Praktikum4_Connected_Database/beranda.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>GymFlex | Beranda</title>
+</head>
+<body>
+
+<div class="navbar">
+    <div class="logo">GYMFLEX</div>
+    <div class="nav-menu">
+        <a href="indexs.php">Beranda</a>
+        <a href="pemesanan.php">Pemesanan</a>
+        <a href="jadwal.php">Jadwal</a>
+        <a href="kontak.php">Kontak</a>
+    </div>
+</div>
+
+<div class="hero">
+    <h1>Bangun Tubuh Ideal,<br>Jadi Versi Terbaik Dirimu</h1>
+    <p>Fasilitas lengkap, pelatih profesional, dan suasana latihan premium—semua dalam satu tempat.</p>
+
+    <div class="btn-box">
+        <a href="pemesanan.php" class="btn">Mulai Sekarang</a>
+        <a href="jadwal.php" class="btn">Lihat Jadwal</a>
+    </div>
+
+    <div class="card">
+        <div class="card-item"><img src="img1.jpg"></div>
+        <div class="card-item"><img src="img2.jpg"></div>
+        <div class="card-item"><img src="img3.jpg"></div>
+    </div>
+</div>
+
+</body>
+</html>
