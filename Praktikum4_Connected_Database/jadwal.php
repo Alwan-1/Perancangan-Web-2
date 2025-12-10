da412608203bf69fb3b78f896e0a26f0405698b1
Alwan-1 <alwanmudhaffar392@gmail.com>
2025-12-10 14:49:23 +0700

    Create jadwal.php

---
+++ Praktikum4_Connected_Database/jadwal.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="UTF-8">
+<title>GymFit - Jadwal</title>
+
+<style>
+body { font-family: Arial; background:#f5f5f5; margin:0; }
+.hero { background:#222; color:white; padding:35px; text-align:center; }
+.navbar { background:#333; text-align:center; }
+.navbar a { color:white; padding:15px 25px; display:inline-block; text-decoration:none; }
+.navbar a:hover { background:#555; }
+.container { width:80%; margin:30px auto; }
+.card { background:white; padding:25px; border-radius:10px; box-shadow:0 4px 12px rgba(0,0,0,0.1); }
+.table { width:100%; border-collapse: collapse; }
+.table th { background:#333; color:white; padding:12px; }
+.table td { padding:12px; background:#fafafa; border-bottom:1px solid #ddd; }
+.footer { background:#222; color:white; text-align:center; padding:15px; margin-top:40px; }
+</style>
+
+</head>
+<body>
+
+<div class="hero">
+    <h1>GymFit</h1>
+    <p>Website Pemesanan Gym Online</p>
+</div>
+
+<div class="navbar">
+    <a href="index.php">Beranda</a>
+    <a href="pemesanan.php">Pemesanan</a>
+    <a href="jadwal.php">Jadwal</a>
+    <a href="kontak.php">Kontak</a>
+    <a href="gambar.php">Gambar</a>
+</div>
+
+<div class="container">
+    <div class="card">
+        <h2>Jadwal Gym</h2>
+
+        <table class="table">
+            <tr>
+                <th>Hari</th>
+                <th>Jam Operasional</th>
+            </tr>
+            <tr>
+                <td>Senin - Jumat</td>
+                <td>08:00 - 20:30</td>
+            </tr>
+            <tr>
+                <td>Sabtu - Minggu</td>
+                <td>08:00 - 20:30</td>
+            </tr>
+        </table>
+
+    </div>
+</div>
+
+<div class="footer">© 2025 GymNess - Salam Sehat ><</div>
+
+</body>
+</html>
